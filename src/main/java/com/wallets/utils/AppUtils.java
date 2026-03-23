9a9fd90af603efa8d0f84fd363744c4a86b67227
bu-kolapo <kolapobukola2019@gmail.com>
2026-03-23 14:30:27 +0100

    application_properties refractored

--- src/main/java/com/wallets/utils/AppUtils.java
+++ src/main/java/com/wallets/utils/AppUtils.java
@@ -5,10 +5,10 @@ import com.wallets.dto.WalletResponse;
 public class AppUtils {
 
     public static WalletResponse walletResponse(int responseCode, boolean success, String message){
-        return WalletResponse.builder()
-                .responseCode(responseCode)
-                .success(success)
-                .message(message)
-                .build();
+        WalletResponse response = new WalletResponse();
+        response.setResponseCode(responseCode);
+        response.setSuccess(success);
+        response.setMessage(message);
+        return response;
     }
 }
